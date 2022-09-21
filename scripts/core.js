b94ecacfd50185062e5bad9a152da4721afdd701
andaran <andrnovip@gmail.com>
2022-09-21 20:17:45 +0300

    test

---
+++ scripts/core.js
@@ -0,0 +1,9 @@
+let gyroscope = new Gyroscope({ frequency: 60 });
+
+gyroscope.addEventListener("reading", (e) => {
+  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+  alert(`Angular velocity along the X-axis ${gyroscope.x}`);
+});
+gyroscope.start();
